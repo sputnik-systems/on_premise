a76c15003e9c064b578f363ac49e271206b6bf4c
Ildar Valiullin <preved.911@gmail.com>
2024-01-12 01:59:01 +0300

    Updated instruction

--- example/RabbitMQ/setup.sh
+++ example/RabbitMQ/setup.sh
@@ -3,5 +3,8 @@
 # docker exec -it rabbitmq rabbitmqctl set_permissions -p / $MQ_USERNAME ".*" ".*" ".*"
 
 # setup pulling events outside
-docker exec -it rabbitmq rabbitmqctl set_parameter federation-upstream spuntik "{\"uri\":\"$MQ_FEDERATION_UPSTREAM_URI\",\"expires\":3600000}"
-docker exec -it rabbitmq rabbitmqctl set_policy --apply-to exchanges user-policy "(^sputnik$)" '{"federation-upstream-set":"all"}'
+docker exec -it rabbitmq rabbitmqctl set_parameter federation-upstream sputnik "{\"uri\":\"$MQ_FEDERATION_UPSTREAM_URI\",\"expires\":3600000}"
+docker exec -it rabbitmq rabbitmqctl set_parameter federation-upstream intercom_missed "{\"uri\":\"$MQ_FEDERATION_UPSTREAM_URI\"}"
+docker exec -it rabbitmq rabbitmqctl set_policy --apply-to exchanges sputnik-input "^(sputnik)$" '{"federation-upstream":"sputnik","alternate-exchange":"intercom_missed"}'
+docker exec -it rabbitmq rabbitmqctl set_policy --apply-to exchanges intercom_missed-input "^(intercom_missed)$" '{"federation-upstream":"intercom_missed"}'
+docker exec -it rabbitmq rabbitmqctl set_policy --apply-to all cleanup "^(missed_queue|logs|webhook-logs)$" '{"max-length-bytes":134217728,"message-ttl":600000}'
